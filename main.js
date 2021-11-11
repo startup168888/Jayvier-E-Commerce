121892368addcaddb24e5f59c8390d6ab3755273
startup168888 <93891138+startup168888@users.noreply.github.com>
2021-11-11 11:56:29 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -209,25 +209,18 @@ function manageQuantity() {
         })
     }
 }
-
-
-/* Displaying Orders while checking out */
-function displayOrder() {
-    let cartItems = localStorage.getItem('productsInCart')
-    document.getElementById("orders").value = "Orders: " + cartItems
-}
-/* Displaying Total while checking out */
-function displayTotal() {
-    let total = localStorage.getItem('totalCost')
-    document.getElementById("total").value = "Total Amount: $"  + total
+function toPaymentPage(){
+    items = localStorage.getItem("productsInCart")
+    if (items !== null) {
+        location.href='payment.html'
+    }else {
+        alert("No items are selected in the Cart")
+    }
 }
 
 
 onLoadCartNumbers();
 displayCart();
-
-displayOrder();
-displayTotal()
 
 
 /* Clear cart button function */
@@ -252,52 +245,8 @@ function hide(){
     })
 }
 
-/* Checkout Button */
-function thanks() {
-    alert('Please Check Your Email for Information' + "\n" + 'Please Proceed to Payment Page');
-    if(localStorage.getItem("totalCost") != null){
-        localStorage.clear();
-        window.location.reload();
-    }
-}
 
-/*Checkout Info*/
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
 
 
 
